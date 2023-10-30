a84927021ff0db0d7a67d94d944288eb47682f76
JuliaS0r <Yulyasor0501@yandex.ru>
2023-10-30 03:13:16 +0300

    проверьте дз по циклам

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,28 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //task1
+        for (int i = 1; i <= 10; i = i + 1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //task2
+        for (int i = 10; i >= 1; i = i - 1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //task3
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //task4
+        for (int i = 10; i >= -10; i = i - 1) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
     }
 }
