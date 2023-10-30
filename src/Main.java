a51666d07007608fbfd39f1c920f93f0d44d726c
JuliaS0r <Yulyasor0501@yandex.ru>
2023-10-30 03:41:04 +0300

    проверьте дз по циклам

--- src/Main.java
+++ src/Main.java
@@ -24,5 +24,44 @@ public class Main {
             System.out.print(i + " ");
         }
         System.out.println();
+
+        //task5
+        for (int i = 1904; i <= 2096; i = i + 4){
+            System.out.println(i + " год является високосным");
+        }
+
+        //task6
+        for (int i = 7; i <= 98; i = i + 7){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //task7
+        for (int i = 1; i <= 512; i = i * 2){
+            System.out.print(i + " ");
+        }
+        System.out.println();
+
+        //task8
+        int salary = 29000;
+        int total = 0;
+        for (int i = 1; i <= 12; i++) {
+            total = total + salary;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total + " рублей");
+        }
+
+        //task9
+        int salary1 = 29000;
+        int total1 = 0;
+        for (int i = 1; i <= 12; i++) {
+            total1 = total1 + total1/100;
+            total1 = total1 + salary1;
+            System.out.println("Месяц " + i + ", сумма накоплений равна " + total1 + " рублей");
+        }
+
+        //task10
+        for (int i = 1; i <= 10; i = i + 1){
+            System.out.println("2 * " + i + " = " + 2 * i);
+        }
     }
 }
